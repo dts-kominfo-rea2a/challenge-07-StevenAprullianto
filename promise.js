73b17c0f88e7e3cf22c206e223421a5341758540
Steven Aprullianto <stevenaprullianto@gmail.com>
2022-06-09 22:07:39 +0700

    Challenge-07

--- promise.js
+++ promise.js
@@ -1,7 +1,49 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async(data) =>{
+    try{
+        let marah = [];
+        let tidakMarah = [];
+        if(data === "marah"){
+          promiseTheaterIXX().then((dataIxx)=>{
+            dataIxx.forEach(element => {
+              if(element.hasil === data){
+                marah.push(element.hasil);
+              }
+            })
+          });
+
+          promiseTheaterVGC().then((dataVgc)=>{
+            dataVgc.forEach(element =>{
+              if(element.hasil === data){
+                marah.push(element.hasil);
+              }
+            })
+            console.log(marah.length);
+          });
+        }else{
+          promiseTheaterIXX().then((dataIxx)=>{
+            dataIxx.forEach(element => {
+              if(element.hasil === data){
+                tidakMarah.push(element.hasil);
+              }
+            })
+          });
+
+          promiseTheaterVGC().then((dataVgc)=>{
+            dataVgc.forEach(element =>{
+              if(element.hasil === data){
+                tidakMarah.push(element.hasil);
+              }
+            })
+            console.log(tidakMarah.length);
+          });
+        }
+    }catch(error){
+      console.log(error);
+    }
+};
 
 module.exports = {
   promiseOutput,
