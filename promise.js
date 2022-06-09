b6b3431c502d8d3d2da5d55ebecf94203e640c82
Steven Aprullianto <stevenaprullianto@gmail.com>
2022-06-09 23:05:49 +0700

    Challenge-07

--- promise.js
+++ promise.js
@@ -1,49 +1,52 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = async(data) =>{
-    try{
-        let marah = [];
-        let tidakMarah = [];
-        if(data === "marah"){
-          promiseTheaterIXX().then((dataIxx)=>{
-            dataIxx.forEach(element => {
-              if(element.hasil === data){
-                marah.push(element.hasil);
-              }
-            })
-          });
+const promiseOutput = (data) =>{
+  return new Promise(
+    (resolve) =>{
+      let arrRespon = []
+      if(data === "marah"){
 
-          promiseTheaterVGC().then((dataVgc)=>{
-            dataVgc.forEach(element =>{
-              if(element.hasil === data){
-                marah.push(element.hasil);
-              }
-            })
-            console.log(marah.length);
-          });
-        }else{
-          promiseTheaterIXX().then((dataIxx)=>{
-            dataIxx.forEach(element => {
-              if(element.hasil === data){
-                tidakMarah.push(element.hasil);
-              }
-            })
-          });
+        promiseTheaterIXX().then((dataIxx)=>{
+          dataIxx.forEach(element => {
+            if(element.hasil === data){
+              arrRespon.push(element.hasil);
+            }
+          })
+        })
 
-          promiseTheaterVGC().then((dataVgc)=>{
-            dataVgc.forEach(element =>{
-              if(element.hasil === data){
-                tidakMarah.push(element.hasil);
-              }
-            })
-            console.log(tidakMarah.length);
-          });
-        }
-    }catch(error){
-      console.log(error);
+        promiseTheaterVGC().then((dataVgc)=>{
+          dataVgc.forEach(element =>{
+            if(element.hasil === data){
+              arrRespon.push(element.hasil);
+            }
+          })
+          resolve(arrRespon.length) ;
+        })
+
+      }else{
+
+        promiseTheaterIXX().then((dataIxx)=>{
+          dataIxx.forEach(element => {
+            if(element.hasil === data){
+              arrRespon.push(element.hasil);
+            }
+          })
+        })
+
+        promiseTheaterVGC().then((dataVgc)=>{
+          dataVgc.forEach(element =>{
+            if(element.hasil === data){
+              arrRespon.push(element.hasil);
+            }
+          })
+          resolve(arrRespon.length) ;
+        })
+
+      }
     }
-};
+  )
+}
 
 module.exports = {
   promiseOutput,
